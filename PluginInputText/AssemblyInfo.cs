cfe55af957fa6b8565de3476d1ea2f7162f3b99b
NaSAeL <89008722+NaSAeL-DWG@users.noreply.github.com>
2025-04-02 15:11:11 +0800

    Add files via upload

---
+++ PluginInputText/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCopyright("© 2010 - Peter Souza IV | Modified by Dynamic Wave Group. 2025 Specifically for TabClass")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion(Rainmeter.Version.Informational)]
+[assembly: AssemblyProduct("Rainmeter")]
